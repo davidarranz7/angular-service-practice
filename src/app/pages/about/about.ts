986c3610b8fe1395c8254f4a3ea1c3e5244711a5
dabid chu <arranzd5@gmail.com>
2026-09-09 10:49:29 +0200

    feat: add user table filters

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,21 +1,35 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, inject, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user-service';
 import { User } from '../../models/user';
 import { Router } from '@angular/router';
-import { MatTableModule } from '@angular/material/table';
 import { MatIcon } from '@angular/material/icon';
+import {
+  MatAutocompleteModule,
+  MatAutocompleteSelectedEvent,
+} from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-about',
-  imports: [CommonModule, MatTableModule, MatIcon],
+  imports: [
+    CommonModule,
+    MatIcon,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
 export class About implements OnInit {
   users = signal<User[]>([]);
-
-  displayedColumns: string[] = ['id', 'username', 'email', 'phone'];
+  nameSearch = signal('');
+  nameFilter = signal('');
+  cityFilter = signal('');
 
   private userService = inject(UserService);
 
@@ -23,6 +37,60 @@ export class About implements OnInit {
 
   goToUserDetail(id: number) {
     this.router.navigate(['/user', id]);
+  }
+
+  filteredUsers = computed(() => {
+    const name = this.nameFilter().toLocaleLowerCase();
+    const city = this.cityFilter().toLocaleLowerCase();
+
+    return this.users().filter((user) => {
+      const matchesName = user.name.toLocaleLowerCase().includes(name);
+      const matchesCity = city == '' || user.address.city.toLocaleLowerCase() == city;
+      return matchesName && matchesCity;
+    });
+  });
+
+  onNameSearchChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const value = inputElement.value;
+
+    this.nameSearch.set(value);
+
+    if (value.trim() === '') {
+      this.nameFilter.set('');
+    }
+  }
+
+  nameSuggestions = computed(() => {
+    const search = this.nameSearch().trim().toLocaleLowerCase();
+
+    if (search === '') {
+      return [];
+    }
+
+    return this.users().filter((user) => user.name.toLocaleLowerCase().includes(search));
+  });
+
+  onNameSelected(event: MatAutocompleteSelectedEvent) {
+    this.nameFilter.set(event.option.value);
+  }
+
+  onNameSearchEnter() {
+    this.nameFilter.set(this.nameSearch().trim().toLocaleLowerCase());
+  }
+
+  cities = computed(() => {
+    return [...new Set(this.users().map((user) => user.address.city))];
+  });
+
+  onCityFilterChange(city: string) {
+    this.cityFilter.set(city);
+  }
+
+  resetFilters() {
+    this.nameSearch.set('');
+    this.nameFilter.set('');
+    this.cityFilter.set('');
   }
 
   ngOnInit(): void {
