d9624ad7a581400decb5307068653bc98074be56
dabid chu <arranzd5@gmail.com>
2026-09-16 10:35:39 +0200

    feat: add GSAP title animation

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, inject } from '@angular/core';
+import { CurrencyPipe, DatePipe, JsonPipe, PercentPipe, SlicePipe } from '@angular/common';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { gsap } from 'gsap';
+
 import { Title } from '../../components/title/title';
 import { Card } from '../../components/card/card';
-import { TranslatePipe, TranslateService } from '@ngx-translate/core';
-import { CurrencyPipe, DatePipe, PercentPipe, SlicePipe, JsonPipe } from '@angular/common';
 import { Auth } from '../../core/services/auth';
 import { SaludoPipe } from '../../pipes/saludo-pipe';
 import { DoblePipe } from '../../pipes/doble-pipe';
@@ -28,10 +30,13 @@ import { EdadPipe } from '../../pipes/edad-pipe';
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {
-  private translateService = inject(TranslateService);
+export class Home implements AfterViewInit {
+  private readonly translateService = inject(TranslateService);
+
   readonly auth = inject(Auth);
   readonly numero = 5;
+
+  @ViewChild('homeTitle') homeTitle!: ElementRef<HTMLDivElement>;
 
   currentDate = new Date();
   subscriptionPrice = 9.99;
@@ -39,5 +44,17 @@ export class Home {
 
   get locale(): string {
     return this.translateService.getCurrentLang() === 'es' ? 'es-ES' : 'en-US';
+  }
+
+  ngAfterViewInit(): void {
+    console.log('HOME GSAP ejecutado');
+
+    gsap.from(this.homeTitle.nativeElement, {
+      y: -80,
+      opacity: 0,
+      duration: 1.2,
+      delay: 0.2,
+      ease: 'power2.out',
+    });
   }
 }
